bba71c3707f1c483577e48369aaccb98724bfde5
buzzkillb <travanx@gmail.com>
2019-03-05 22:52:19 -0800

    Create denariusd.sh

---
+++ denariusd.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+echo "this assumes you have dependencies already"
+sleep 10
+
+cd ~/Downloads
+wget https://triforce.black/d/denariusd
+chmod +x denariusd
+sudo cp denariusd /usr/local/bin/denariusd
+
+echo "to run daemon type denariusd"
+echo "if this doesn't work try installing dependencies"
+echo "sudo apt-get --assume-yes install git unzip build-essential libssl-dev libdb++-dev libboost-all-dev libqrencode-dev libminiupnpc-dev libgmp-dev libevent-dev autogen automake libtool"
